56564e9983bcc5f9211ce0818ddc592b5222ffac
CptSpaceToaster <CptSpaceToaster@gmail.com>
2015-10-29 20:14:49 -0400

    Add smoke test.  Fix Algo.  Multithread

--- aecko/blend.py
+++ aecko/blend.py
@@ -13,9 +13,9 @@ Written in a wonderful flurry of activitiy with contributions from
 """
 # System
 import os
+import glob
 import argparse
-# TODO: Re-multithread this
-# import concurrent.futures
+import concurrent.futures
 import natsort
 import numpy
 from PIL import Image
@@ -24,22 +24,21 @@ from PIL import Image
 import aecko
 
 
-def diff(val1, val2, center=255):
-    return int((center + val2 - val1)/2)
-
-
 def do_compare(pic1, pic2):
     # TODO: What if pic1 and pic2 are actually different dimensions for some reason?
-    ret = numpy.empty(pic1.shape, pic1.dtype)
-    for (i1, p1), (i2, p2) in zip(numpy.ndenumerate(pic1), numpy.ndenumerate(pic2)):
-        # print(i1, p1, end=', ')
-        # print(i2, p2)
-        # Skip the alpha component of each pixel
-        if i1[2] == 3:
-            ret[i1] = 255
-            continue
-        ret[i1] = (diff(p1, p2))
-    return ret
+    # print(pic1.shape, pic1.dtype)
+    # print(pic2.shape, pic2.dtype)
+    if pic1.shape[2] == 3:
+        return (((pic2 + 255 - pic1)//numpy.array([2, 2, 2]))).view('uint8')[:, :, ::8]
+    else:
+        return (((pic2 + 255 - pic1)//numpy.array([2, 2, 2, 1]))).view('uint8')[:, :, ::8]
+
+
+def chunks(l, n):
+    w = len(l)//n
+    for i in range(0, n-1):
+        yield l[i*w:i*w+w]
+    yield l[n*w-w:]
 
 
 # Main code (also make it look like C? :p)
@@ -58,18 +57,21 @@ def main():
     # Create the output directory if it doesn't exist already
     os.makedirs(args.output_path, exist_ok=True)
 
-    # Fetch the frames
-    files = os.listdir(args.image_path)
-    print('Found {0} files!'.format(len(files)))
+    # Fetch the frames in a sorted fashion
+    files = natsort.natsorted(glob.glob('*.png'))
+    num_files = len(files)
+    print('Found {0} files!'.format(num_files))
 
     # Initialize the threadpool
+    pool = concurrent.futures.ThreadPoolExecutor(args.worker_no)
+
+    offset = 0
+    for chunk in chunks(files, args.worker_no):
+        pool.submit(walk, chunk, offset, args.output_path)
+        offset += num_files//args.worker_no
+
     # TODO: Re-multithread this
-    # pool = concurrent.futures.ThreadPoolExecutor(args.worker_no)
-    # Add another job to the threadpool!  It will munch through all of the jobs we give it as time passes
-    # TODO: Re-multithread this
-    # pool.submit(do_compare, last_file, filename, '{0}/{1}-{2}_compare.png'.format(args.output_path, frame_no, frameNo + 1))
-    # do_compare(last_file, filename, '{0}/{1}-{2}_compare.png'.format(args.output_path, frame_no, frameNo + 1))
-    walk(files, 0, args.output_path)
+    pool.shutdown(wait=True)
 
 
 def walk(files, offset, output_path):
@@ -79,29 +81,27 @@ def walk(files, offset, output_path):
     # Variable to keep track of the last frame in sequence
     last_file = None
     last_pic = None
+    curr_pic = None
 
     for current_file in natsort.natsorted(files):
+        curr_pic = numpy.array(Image.open(current_file), dtype=numpy.int16)
+
         # Skip the first frame, because there's no previous image (check that first frame is an image)
         if not last_file and current_file.endswith('.png'):
             last_file = current_file
-            last_pic = numpy.array(Image.open(current_file))
+            last_pic = curr_pic
             continue
 
         # Do image diff processing
-        if current_file.endswith('.png'):
-            out_file = '{0}/{1}-{2}_compare.png'.format(output_path, frame_no, frame_no + 1)
-            print('Processing: {0} vs {1}...'.format(last_file, current_file))
+        out_file = '{0}/{1}-{2}_compare.png'.format(output_path, frame_no, frame_no + 1)
+        print('Processing: {0} vs {1}...'.format(last_file, current_file))
 
-            pic = do_compare(last_pic, numpy.array(Image.open(current_file)))
-            Image.fromarray(pic).save(out_file)
+        Image.fromarray(do_compare(last_pic, curr_pic)).save(out_file)
 
-            # Update the last file and frame count variables
-            last_file = current_file
-            frame_no += 1
-
-    # TODO: Re-multithread this
-    # pool.shutdown(wait=True)
-    print('Finished!')
+        # Update the last file and frame count variables
+        last_file = current_file
+        last_pic = curr_pic
+        frame_no += 1
 
 
 if __name__ == '__main__':
--- aecko/test/test_example.py
+++ aecko/test/test_example.py
@@ -1,18 +1,74 @@
 import unittest
+import numpy
+
+from aecko import blend
 
 
 class ExampleTestClass(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_example(self):
-        """
-        This function name MUST begin with 'test'
-        """
-        self.assertEqual(1 + 2, 3)
+    def test_compare(self):
+        a = numpy.array([[[127, 127, 127],
+                          [127, 127, 127]],
+                         [[127, 127, 127],
+                          [127, 127, 127]]],
+                        dtype=numpy.int16)
 
-    def test_example_another(self):
-        self.assertNotEqual(1, '1')
+        b = numpy.array([[[127, 127, 127],
+                          [255, 0,   0]],
+                         [[0,   255, 0],
+                          [0,   0,   255]]],
+                        dtype=numpy.int16)
+
+        c = numpy.array([[[127, 127, 127],
+                          [191, 64,  64]],
+                         [[64,  191, 64],
+                          [64,  64,  191]]])
+
+        d = numpy.array([[[127, 127, 127],
+                          [63,  191, 191]],
+                         [[191, 63,  191],
+                          [191, 191, 63]]])
+
+        e = blend.do_compare(a, b)
+        f = blend.do_compare(b, a)
+
+        numpy.testing.assert_equal(e, c)
+        self.assertEqual(e.dtype, numpy.uint8)
+        numpy.testing.assert_equal(f, d)
+        self.assertEqual(f.dtype, numpy.uint8)
+
+    def test_compare_with_alpha(self):
+        a = numpy.array([[[127, 127, 127, 255],
+                          [127, 127, 127, 255]],
+                         [[127, 127, 127, 255],
+                          [127, 127, 127, 255]]],
+                        dtype=numpy.int16)
+
+        b = numpy.array([[[127, 127, 127, 255],
+                          [255, 0,   0,   255]],
+                         [[0,   255, 0,   255],
+                          [0,   0,   255, 255]]],
+                        dtype=numpy.int16)
+
+        c = numpy.array([[[127, 127, 127, 255],
+                          [191, 64,  64,  255]],
+                         [[64,  191, 64,  255],
+                          [64,  64,  191, 255]]])
+
+        d = numpy.array([[[127, 127, 127, 255],
+                          [63,  191, 191, 255]],
+                         [[191, 63,  191, 255],
+                          [191, 191, 63,  255]]])
+
+        e = blend.do_compare(a, b)
+        f = blend.do_compare(b, a)
+
+        numpy.testing.assert_equal(e, c)
+        self.assertEqual(e.dtype, numpy.uint8)
+        numpy.testing.assert_equal(f, d)
+        self.assertEqual(f.dtype, numpy.uint8)
 
     def tearDown(self):
         pass
